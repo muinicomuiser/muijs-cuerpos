0a26fb4d566b447c2d784424e943e8098de5f2b0
muinicomuiser <nicodoneg@gmail.com>
2025-01-14 13:04:15 -0300

    Corrección exportaciones

---
+++ Fuente/mui.ts
@@ -0,0 +1,38 @@
+/**Utiles*/
+export { Matematica } from "./Utiles/Matematica";
+export { Geometria } from "./Utiles/Geometria";
+export { Grabador } from "./Utiles/Grabacion";
+
+/**Geometría Plana*/
+export { Forma } from "./GeometriaPlana/Formas";
+export { Punto } from "./GeometriaPlana/Punto";
+export { TipoFormas } from "./GeometriaPlana/TipoFormas";
+export { Vector } from "./GeometriaPlana/Vector";
+export { OpcionesForma } from "./GeometriaPlana/OpcionesForma"
+
+/**Interacción*/
+export { Colision } from "./Interaccion/Colision";
+export { Entorno } from "./Interaccion/Entorno";
+export { Interaccion } from "./Interaccion/Interaccion";
+export { Restriccion } from "./Interaccion/Restriccion";
+
+/**Físicas*/
+export { Cinematica } from "./Fisicas/Cinematica";
+export { Cuerpo } from "./Fisicas/Cuerpo";
+export { Fuerza } from "./Fisicas/Fuerza";
+export { OpcionesCuerpo } from "./Fisicas/OpcionesCuerpo"
+export { Contenedor } from "./Fisicas/Contenedor";
+
+/**Renderizado*/
+export { Dibujante } from "./Renderizado/Dibujante";
+export { Renderizado } from "./Renderizado/Renderizado";
+
+/**Composición*/
+export { Composicion } from "./Composicion/Composicion";
+export { ManejadorEventos } from "./Composicion/ManejadorEventos"
+export { Tiempo } from "./Composicion/Tiempo";
+
+/**Cuadrícula*/
+export { Celda } from "./Cuadricula/Celda"
+export { Cuadricula } from "./Cuadricula/Cuadricula"
+export { Temporizador } from "./Composicion/Temporizador"
